faa0240f3e461d3e68ca391d9eeec42acdc30f6e
Mohamed amine Baioumy <mbaioumy@e2r11p11.1337.ma>
2022-02-27 19:57:44 +0100

    fixes

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 01:13:26 by mbaioumy          #+#    #+#             */
-/*   Updated: 2022/02/24 03:18:08 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2022/02/27 19:41:25 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,24 +38,23 @@ char	*findthewae(char **str)
     return ("zbi");
 }
 
-void	execute(char **env, char *cmd, char *cmdArg)
+void	execute(char **env, char *cmd)
 {
 	char	*path;
 	char	**cmdPaths;
 	int		i;
 	char	*arg;
+	char	**cmda;
 
 	i = -1;
 	path = findthewae(env);
 	cmdPaths = ft_split(path, ':');
+	cmda = ft_split(cmd, ' ');
 	while (cmdPaths[++i])
 	{
-		//printf("%s\n", cmdPaths[i]);
-		//printf("%s", cmd);
 		arg = ft_strjoin(cmdPaths[i], "/");
-		arg = ft_strjoin(arg, cmd);
-		//printf("%s\n", arg);
-		execve(arg, &cmdArg, env);
+		arg = ft_strjoin(arg, cmda[0]);
+		execve(arg, cmda, env);
 	}
 }
 
@@ -63,51 +62,45 @@ void	pipex(int file1, int file2, char *cmd1, char *cmd2, char **env)
 {
 	int fd[2];
 	int pid1;
-	int cid1;
-	int	p;
 
-	//p = pipe(fd);
-	//if (p < 0)
-		//return ;
+	if (pipe(fd) < 0)
+		return ;
 	pid1 = fork();
-	//cid1 = getpid();
-	//if (pid1 == -1)
-	//	return ;
-	//printf("pid1 %d/n", pid1);
+	if (pid1 == -1)
+		return ;
 	if (pid1 == 0)
 	{
-		//printf("im here");
-		//dup2(fd[1], STDOUT_FILENO);
-		//close(file1);
-		//close(fd[0]);
-		//close(fd[1]);
-		execute(env, cmd1, cmd2);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execute(env, cmd1);
 	}
-
-	// int pid2 = fork();
-	// if (pid2 < 0)
-	// 	return ;
-	// if (pid2 == 0)
-	// {
-	// 	dup2(fd[0], STDIN_FILENO);
-	// 	close(fd[0]);
-	// 	close(fd[1]);
-	// 	execute(env, cmd2);
-	// }
+	close(file1);
+	int pid2 = fork();
+	if (pid2 < 0)
+		return ;
+	if (pid2 == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execute(env, cmd2);
+	}
+	close(file2);
 	close(fd[0]);
 	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	//waitpid(pid2, NULL, 0);
+	wait(0);
 }
 
 int main(int argc, char **argv, char **env)
 {
-	// int file1, file2;
+	int file1, file2;
 
-	// file1 = open(argv[1], O_RDONLY);
-	// file2 = open(argv[4], O_CREAT | O_RDWR, 0777);
-	// if (file1 < 0 || file2 < 0)
-	// 	return (-1);
-	pipex(42, 42, argv[1], argv[2], env);
+	file1 = open(argv[1], O_RDONLY);
+	file2 = open(argv[4], O_CREAT | O_RDWR, 0777);
+	if (file1 < 0 || file2 < 0)
+		return (-1);
+	pipex(file1, file2, argv[2], argv[3], env);
+	//system("a out leaks");
 	return (0);
 }
